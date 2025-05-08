688ec421bd8d3dbf9db62ef572959f6bae950cb7
HumbleOp <fra.cossu.chem@gmail.com>
2025-05-08 18:07:45 +0200

    Update config.py

--- app/config/config.py
+++ app/config/config.py
@@ -5,5 +5,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, '../../humbleop.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    USERS_FILE = os.path.join(BASE_DIR, '../../users.json')
-    DATA_FILE = os.path.join(BASE_DIR, '../../data.json')
+
+    # Absolute paths for USERS_FILE and DATA_FILE
+    USERS_FILE = os.path.abspath(os.path.join(BASE_DIR, '../../users.json'))
+    DATA_FILE = os.path.abspath(os.path.join(BASE_DIR, '../../data.json'))
