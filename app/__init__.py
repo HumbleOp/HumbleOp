cbedd1da3b023bb3d3e85d0b5e728db885751301
HumbleOp <fra.cossu.chem@gmail.com>
2025-05-10 09:44:17 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,7 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-# Initialize the SQLAlchemy object
-db = SQLAlchemy()
+from .extensions import db
 
 def create_app():
-    """Application factory for creating the Flask app."""
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Initialize db with app
+    app.config.from_object("config.Config")
     db.init_app(app)
-
     return app
